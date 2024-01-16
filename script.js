f66368d168fc8d79c96021ce173fc0d12dcd585f
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-01-16 16:31:25 -0600

    ultimo commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let options = {
+    possiblewords: [
+        "ESPAÑA",
+        "ECUADOR",
+        "RUSIA",
+        "VENEZUELA",
+        "GUATEMALA",
+        "IRAN",
+        "ERIZO", 
+        "RINOCERONTE", 
+        "ARDILLA", 
+        "PANTERA", 
+        "MORSA", 
+        "ZEBRA",
+        "India",
+        "Mexico",
+        "Honduras",
+        "Panama",
+        "Belice",
+        "BRASIL",]
+};
+
+
+
+
+
+
